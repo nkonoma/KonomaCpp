5c76daa396fef5f5ad2a762f5c973156cb074582
Nicolas Konoma <nic.gundam00@gmail.com>
2024-06-26 13:33:44 -0400

    Initial Commit

---
+++ KonomaCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KonomaCpp
+//
+//  Created by Nic Konoma on 6/26/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
